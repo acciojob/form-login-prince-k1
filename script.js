096cb748446894ee5ab2f9b923c83d5e618174ca
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-09-21 01:50:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+	const form = document.getElementById("form1");
+	const firstName = form[0].value;
+	const lastName = form[1].value;
+	alert(firstName+" "+ lastName);
 
 }
